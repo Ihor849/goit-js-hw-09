2675017c57665b22a6bab839a4dffca6b82862f4
Ihor849 <333marchelo333@gmail.com>
2023-02-28 01:33:33 +0200

    виправля помилки 28.02

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,33 +3,31 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   formEl: document.querySelector('.form'),
-  inputDelay: document.querySelector('input[name="delay"]'),
-  inputStep: document.querySelector('input[name="step"]'),
-  inputAmount: document.querySelector('input[name="amount"]'),
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
   button: document.querySelector('button[type="submit"]'),
 };
-// refs.formEl.addEventListener('input', e => {
-//   console.log(e);
-// });
 
 refs.formEl.addEventListener('submit', submitCreatePromise);
 
 function submitCreatePromise(e) {
   e.preventDefault();
 
-  const delayStep = Number(refs.inputDelay.value);
-  let delayFirst = Number(refs.inputStep.value);
-  const amount = Number(refs.inputAmount.value);
+  let delay = Number(refs.delay.value);
+  const step = Number(refs.step.value);
+  const amount = Number(refs.amount.value);
 
   for (let i = 1; i <= amount; i += 1) {
-    createPromise(i, delayFirst)
-      .then(({ position, delayFirst }) => {
-        Notiflix.Notify.success(`✅ Fulfilled promise ${i} in ${delayFirst}ms`);
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${i} in ${delay} ms`);
+        console.log(delay);
       })
-      .catch(({ position, delayFirst }) => {
-        Notiflix.Notify.failure(`❌ Rejected promise ${i} in ${delayFirst}ms`);
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${i} in ${delay} ms`);
       });
-    delayFirst += delayStep;
+    delay += step;
   }
 }
 
